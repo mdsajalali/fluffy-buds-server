0aaa664e90f52b5312669712c09325a161241355
mdsajalali <mdsajalali2020@gmail.com>
2025-04-13 23:46:33 +0600

    added: product create functionality

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,4 +1,33 @@
+import productModel from "../models/product.model.js";
+
 // create product
-const createProduct = async (req, res) => {};
+const createProduct = async (req, res) => {
+  let images = req.body.filename;
+  try {
+    const { name, description, price, discount, category, sizes, colors } =
+      req.body;
+
+    const product = new productModel({
+      name,
+      description,
+      price,
+      discount,
+      category,
+      sizes,
+      colors,
+      images,
+    });
+
+    await product.save();
+    res.json({
+      success: true,
+      message: "Product created successfully",
+      product,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error creating product" });
+  }
+};
 
 export { createProduct };

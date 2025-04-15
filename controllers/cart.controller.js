f1b7afcddd432d40ad1d56eeac3c27c321f2d703
mdsajalali <mdsajalali2020@gmail.com>
2025-04-15 18:04:11 +0600

    added: remove from cart functionality

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -26,4 +26,28 @@ const addToCart = async (req, res) => {
   }
 };
 
-export { addToCart };
+
+// remove from cart
+const removeFromCart = async (req, res) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+
+    if (cartData[req.body.itemId] > 0) {
+      cartData[req.body.itemId] -= 1;
+    }
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.json({
+      success: false,
+      message: "Product Removed From Cart",
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: "Failed to remove product from cart.",
+    });
+  }
+};
+
+export { addToCart, removeFromCart };
--- routes/cart.route.js
+++ routes/cart.route.js
@@ -1,8 +1,9 @@
 import express from "express";
+import { addToCart, removeFromCart } from "../controllers/cart.controller.js";
 import authMiddleware from "../middleware/auth.js";
-import { addToCart } from "../controllers/cart.controller.js";
 const cartRoute = express.Router();
 
 cartRoute.post("/add", authMiddleware, addToCart);
+cartRoute.post("/remove", authMiddleware, removeFromCart);
 
 export default cartRoute;
